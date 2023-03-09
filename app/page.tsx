0eab1b1fbc2347fc4de460580a88a4d16d67ced4
Alek Novkovski <alek@greatcode2.com>
2023-03-09 22:23:56 +0100

    Move top-level markup and navbar into Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,19 @@
-import NavBar from "@/app/components/NavBar";
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
 
 export default function Home() {
     return (
-        <main className="bg-gray-100 min-h-screen w-screen">
-            <main className="max-w-screen-2xl m-auto bg-white">
-                {/* NAVBAR */}
-                <NavBar/>
-                {/* NAVBAR */}
-                <main>
-                    {/* HEADER */}
-                    <Header />
-                    {/* HEADER */}
-                    {/* CARDS */}
-                    <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-                        {/* CARD */}
-                        <RestaurantCard />
-                        {/* CARD */}
-                    </div>
-                    {/* CARDS */}
-                </main>
-            </main>
-        </main>
+        <>
+            {/* HEADER */}
+            <Header/>
+            {/* HEADER */}
+            {/* CARDS */}
+            <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+                {/* CARD */}
+                <RestaurantCard/>
+                {/* CARD */}
+            </div>
+            {/* CARDS */}
+        </>
     )
 }
