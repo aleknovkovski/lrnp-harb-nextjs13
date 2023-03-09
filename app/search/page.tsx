d601bb7b05f2c8dc88a69180f973637d8adb387c
Alek Novkovski <alek@greatcode2.com>
2023-03-09 21:58:30 +0100

    Utilize SearchBar in searchPage header

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/app/components/NavBar";
 import {SearchSidebar} from "@/app/search/components/SearchSidebar";
 import {RestaurantCard} from "@/app/search/components/RestaurantCard";
+import {SearchBar} from "@/app/components/SearchBar";
 
 export default function SearchPage() {
     return (
@@ -10,16 +11,7 @@ export default function SearchPage() {
                 <NavBar/>
                 {/* HEADER */}
                 <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-                    <div className="text-left text-lg py-3 m-auto flex justify-center">
-                        <input
-                            className="rounded  mr-3 p-2 w-[450px]"
-                            type="text"
-                            placeholder="State, city or town"
-                        />
-                        <button className="rounded bg-red-600 px-9 py-2 text-white">
-                            Let's go
-                        </button>
-                    </div>
+                    <SearchBar/>
                 </div>
                 <div className="flex py-4 m-auto w-2/3 justify-between items-start">
                     {/* SEARCH SIDE BAR */}
@@ -33,5 +25,5 @@ export default function SearchPage() {
                 </div>
             </main>
         </main>
-    )
+)
 }
