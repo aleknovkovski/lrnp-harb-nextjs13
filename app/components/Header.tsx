22bcf7818c9920213e9344e42cf8c72632f5863d
Alek Novkovski <alek@greatcode2.com>
2023-03-09 21:49:48 +0100

    Move SearchBar into separate client component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,10 +2,9 @@
 
 import {useState} from "react";
 import {useRouter} from "next/navigation";
+import {SearchBar} from "@/app/components/SearchBar";
 
 export default function Header() {
-    const [location, setLocation] = useState("")
-    const router = useRouter();
 
     return (
         <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
@@ -14,20 +13,7 @@ export default function Header() {
                     Find your table for any occasion
                 </h1>
                 {/* SEARCH BAR */}
-                <div className="text-left text-lg py-3 m-auto flex justify-center">
-                    <input
-                        className="rounded  mr-3 p-2 w-[450px]"
-                        type="text"
-                        placeholder="State, city or town"
-                        value={location}
-                        onChange={(e) => setLocation(e.target.value)}
-                    />
-                    <button
-                        className="rounded bg-red-600 px-9 py-2 text-white"
-                        onClick={()=> router.push("/search")}>
-                        Let's go
-                    </button>
-                </div>
+                <SearchBar/>
                 {/* SEARCH BAR */}
             </div>
         </div>
