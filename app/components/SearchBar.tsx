1483ada887710621c21e1dee6d4f6f35ea25b5fd
Alek Novkovski <alek@greatcode2.com>
2023-03-10 23:39:36 +0100

    Pass search input to url query params

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -3,9 +3,18 @@
 import {useState} from "react";
 import {useRouter} from "next/navigation";
 
+
+
 export function SearchBar() {
     const [location, setLocation] = useState("")
     const router = useRouter();
+
+    function searchHandler(): void {
+        if(location) {
+            router.push(`/search?location=${location}`)
+            setLocation('')
+        }
+    }
 
     return (
         <div className="text-left text-lg py-3 m-auto flex justify-center">
@@ -18,7 +27,7 @@ export function SearchBar() {
             />
             <button
                 className="rounded bg-red-600 px-9 py-2 text-white"
-                onClick={() => router.push("/search")}>
+                onClick={searchHandler}>
                 Let's go
             </button>
         </div>
