1cf6c43865b216e693a4cc9947124d3740942b46
Alek Novkovski <alek@greatcode2.com>
2023-03-10 22:01:50 +0100

    Fetch a single restaurant's items, console.log as menu

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,10 +1,31 @@
 import {Header} from "@/app/restaurant/[slug]/components/Header";
 import {RestaurantNavbar} from "@/app/restaurant/[slug]/components/RestaurantNavbar";
 import {Menu} from "@/app/restaurant/[slug]/components/Menu";
+import {PrismaClient} from "@prisma/client";
 
-export default function MenuPage(
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+
+  const restaurant = await prisma.restaurant.findFirst({
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
+};
+
+export default async function MenuPage(
     {params}: { params: { slug: string }}
 ) {
+    const menu = await fetchRestaurantMenu(params.slug);
+    console.log(menu)
+
     return (
         <>
             <div className="bg-white w-[100%] rounded p-3 shadow">
