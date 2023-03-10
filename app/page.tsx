3797f49aad5aa7740fa1f535e8c76792f11132f2
Alek Novkovski <alek@greatcode2.com>
2023-03-10 18:29:42 +0100

    Fetch all restaurants from db, console.log

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
+import {PrismaClient} from "@prisma/client";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+async function fetchRestaurants() {
+  const restaurants = await prisma.restaurant.findMany();
+  return restaurants;
+};
+
+export default async function Home() {
+    console.log(await fetchRestaurants())
     return (<>
         <Header/>
         <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
