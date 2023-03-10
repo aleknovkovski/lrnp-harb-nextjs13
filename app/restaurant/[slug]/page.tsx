8007e08d97e93ae40e72f926f034cbea9893df07
Alek Novkovski <alek@greatcode2.com>
2023-03-10 20:26:51 +0100

    Handle typescript return

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -14,12 +14,31 @@ export const metadata = {
 
 const prisma = new PrismaClient();
 
-const fetchRestaurantBySlug = async (slug: string) => {
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
   const restaurant = await prisma.restaurant.findUnique({
     where: {
       slug,
     },
+      select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
   });
+
+  if(!restaurant) {
+      throw new Error()
+  }
 
   return restaurant;
 };
