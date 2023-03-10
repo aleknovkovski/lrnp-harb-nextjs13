765a23682094a6b511e0b9292377216e54f6a21d
Alek Novkovski <alek@greatcode2.com>
2023-03-10 18:51:17 +0100

    Define RestaurantCardType interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
+
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+}
 
 const prisma = new PrismaClient();
 
