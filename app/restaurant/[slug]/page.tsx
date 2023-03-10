9817772923addf5b84137e78f7b2c05195d2d89d
Alek Novkovski <alek@greatcode2.com>
2023-03-10 20:06:11 +0100

    Use slug to fetch restaurant from db

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,17 +6,31 @@ import {Description} from "@/app/restaurant/[slug]/components/Description";
 import {Images} from "@/app/restaurant/[slug]/components/Images";
 import {Reviews} from "@/app/restaurant/[slug]/components/Reviews";
 import {ReservationCard} from "@/app/restaurant/[slug]/components/ReservationCard";
+import {PrismaClient} from "@prisma/client";
 
 export const metadata = {
     title: 'Milestones Grill | Open Table',
     description: 'Milestones Grill | Open Table',
 }
 
-export default function RestaurantPage(
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  return restaurant;
+};
+
+export default async function RestaurantPage(
     {params}: {params: { slug: string } }
     ) {
 
-    console.log(params);
+    console.log(await fetchRestaurantBySlug(params.slug))
+
     return (
         <>
                     <div className="bg-white w-[70%] rounded p-3 shadow">
