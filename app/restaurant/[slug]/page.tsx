c3058e669fddafe64d1b2ed0062755d7d9f70d65
Alek Novkovski <alek@greatcode2.com>
2023-03-10 20:07:33 +0100

    Remove comments, unused import

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import {Header} from "@/app/restaurant/[slug]/components/Header";
 import {RestaurantNavbar} from "@/app/restaurant/[slug]/components/RestaurantNavbar";
 import {Title} from "@/app/restaurant/[slug]/components/Title";
 import {Rating} from "@/app/restaurant/[slug]/components/Rating";
@@ -34,24 +33,12 @@ export default async function RestaurantPage(
     return (
         <>
                     <div className="bg-white w-[70%] rounded p-3 shadow">
-                        {/* RESTAURANT NAVBAR */}
                         <RestaurantNavbar/>
-                        {/* RESTAURANT NAVBAR */}
-                        {/* TITLE */}
                         <Title/>
-                        {/* TITLE */}
-                        {/* RATING */}
                         <Rating/>
-                        {/* RATING */}
-                        {/* DESCRIPTION */}
                         <Description/>
-                        {/* DESCRIPTION */}
-                        {/* IMAGES */}
                         <Images/>
-                        {/* IMAGES */}
-                        {/* REVIEWS */}
                         <Reviews/>
-                        {/* REVIEWS */}
                     </div>
                     <div className="w-[27%] relative text-reg">
                         <ReservationCard/>
