3ae7161ea9b013e4e1241d462c438f7a6d997e90
Alek Novkovski <alek@greatcode2.com>
2023-03-10 21:01:28 +0100

    Display restaurant name in layout header, dynamically

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,14 +1,18 @@
 import {Header} from "@/app/restaurant/[slug]/components/Header";
 
 export default function RestaurantLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
 }) {
-    return (<>
-        <Header/>
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-            {children}
-        </div>
-    </>)
+  return (
+    <main>
+      <Header name={params.slug} />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        {children}
+      </div>
+    </main>
+  );
 }
