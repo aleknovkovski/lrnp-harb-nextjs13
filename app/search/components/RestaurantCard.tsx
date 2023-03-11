d3fdfbddd1df233fec1ff3319e8ea2dd7747c751
Alek Novkovski <alek@greatcode2.com>
2023-03-11 01:31:43 +0100

    Add interface to props

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,28 +1,40 @@
 import Link from "next/link";
 import Price from "@/app/components/Price";
+import {Cuisine, PRICE, Location} from "@prisma/client";
 
-export function RestaurantCard(props: any) {
+interface Restaurant {
+  id: number;
+  name: string;
+  main_image: string;
+  price: PRICE;
+  cuisine: Cuisine;
+  location: Location;
+  slug: string;
+}
+
+export function RestaurantCard({restaurant}: {restaurant: Restaurant} ) {
+
     return <div className="border-b flex pb-5">
         <img
-            src={props.restaurant.main_image}
+            src={restaurant.main_image}
             alt=""
             className="w-44 rounded"
         />
         <div className="pl-5">
-            <h2 className="text-3xl">{props.restaurant.name}</h2>
+            <h2 className="text-3xl">{restaurant.name}</h2>
             <div className="flex items-start">
                 <div className="flex mb-2">*****</div>
                 <p className="ml-2 text-sm">Awesome</p>
             </div>
             <div className="mb-9">
                 <div className="font-light flex text-reg">
-                    <Price price={props.restaurant.price} />
-                    <p className="mr-4">{props.restaurant.cuisine.name}</p>
-                    <p className="mr-4">{props.restaurant.location.name}</p>
+                    <Price price={restaurant.price} />
+                    <p className="mr-4">{restaurant.cuisine.name}</p>
+                    <p className="mr-4">{restaurant.location.name}</p>
                 </div>
             </div>
             <div className="text-red-600">
-                <Link href={`/restaurant/${props.restaurant.slug}`}>View more information</Link>
+                <Link href={`/restaurant/${restaurant.slug}`}>View more information</Link>
             </div>
         </div>
     </div>;
