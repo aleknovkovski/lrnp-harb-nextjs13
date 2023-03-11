eac290c84bba95a07aa77366ecc282782998924e
Alek Novkovski <alek@greatcode2.com>
2023-03-11 11:16:22 +0100

    Dynamically append clicked cuisine, restaraunt to query

--- app/search/components/SearchSidebar.tsx
+++ app/search/components/SearchSidebar.tsx
@@ -1,23 +1,47 @@
 import {Cuisine, Location, PRICE} from "@prisma/client";
 import Link from "next/link";
 
-export function SearchSidebar({locations, cuisines, searchParams}: { locations: Location[], cuisines: Cuisine[], searchParams: { city?: string; cuisine?: string; price?: PRICE }; }) {
+export function SearchSidebar({
+                                  locations,
+                                  cuisines,
+                                  searchParams
+                              }: { locations: Location[], cuisines: Cuisine[], searchParams: { city?: string; cuisine?: string; price?: PRICE }; }) {
     console.log(searchParams)
     return <div className="w-1/5">
-        <div className="border-b pb-4">
+        <div className="border-b pb-4 flex flex-col">
             <h1 className="mb-2">Region</h1>
             {locations.map((location) => (
-                <p className="font-light text-reg capitalize" key={location.id}>
+                <Link
+                    className="font-light text-reg capitalize"
+                    key={location.id}
+                    href={{
+                        pathname: "/search",
+                        query: {
+                            ...searchParams,
+                            location: location.name,
+                        },
+                    }}
+                >
                     {location.name}
-                </p>
+                </Link>
             ))}
         </div>
-        <div className="border-b pb-4 mt-3">
+        <div className="border-b pb-4 flex flex-col">
             <h1 className="mb-2">Cuisine</h1>
             {cuisines.map((cuisine) => (
-                <p className="font-light text-reg capitalize" key={cuisine.id}>
+                <Link
+                    className="font-light text-reg capitalize"
+                    key={cuisine.id}
+                    href={{
+                        pathname: "/search",
+                        query: {
+                            ...searchParams,
+                            cuisine: cuisine.name,
+                        },
+                    }}
+                >
                     {cuisine.name}
-                </p>
+                </Link>
             ))}
         </div>
         <div className="mt-3 pb-4">
