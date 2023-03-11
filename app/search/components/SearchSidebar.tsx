4b119347da110e1acaefa7d8629c151c1b7c4d19
Alek Novkovski <alek@greatcode2.com>
2023-03-11 11:25:06 +0100

    Dynamically append clicked price to query

--- app/search/components/SearchSidebar.tsx
+++ app/search/components/SearchSidebar.tsx
@@ -10,7 +10,24 @@ export function SearchSidebar({
     cuisines: Cuisine[],
     searchParams: { city?: string; cuisine?: string; price?: PRICE };
 }) {
-    console.log(searchParams)
+      const prices = [
+    {
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "border w-full text-reg text-center font-light rounded-l p-2",
+    },
+    {
+      price: PRICE.REGULAR,
+      label: "$$",
+      className: "border w-full text-reg text-center font-light p-2",
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className: "border w-full text-reg text-center font-light rounded-r p-2",
+    },
+  ];
+
     return <div className="w-1/5">
         <div className="border-b pb-4 flex flex-col">
             <h1 className="mb-2">Region</h1>
@@ -51,19 +68,21 @@ export function SearchSidebar({
         <div className="mt-3 pb-4">
             <h1 className="mb-2">Price</h1>
             <div className="flex">
-                <button className="border w-full text-reg font-light rounded-l p-2">
-                    $
-                </button>
-                <button
-                    className="border-r border-t border-b w-full text-reg font-light p-2"
+            {prices.map((price, index) => (
+                <Link
+                    className={price.className}
+                    key={index}
+                    href={{
+                        pathname: "/search",
+                        query: {
+                            ...searchParams,
+                            price: price.price,
+                        },
+                    }}
                 >
-                    $$
-                </button>
-                <button
-                    className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r"
-                >
-                    $$$
-                </button>
+                    {price.label}
+                </Link>
+            ))}
             </div>
         </div>
     </div>;
