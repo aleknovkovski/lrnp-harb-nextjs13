bb136a651975985f95098e3477ffd53fdc0eb473
Alek Novkovski <alek@greatcode2.com>
2023-03-11 19:29:38 +0100

    Add Stars into HomePage cards

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {RestaurantCardType} from "@/app/page";
 import Price from "@/app/components/Price";
+import Stars from "@/app/components/Stars";
 
 interface Props {
     restaurant: RestaurantCardType
@@ -20,7 +21,7 @@ export default function RestaurantCard ({restaurant}: Props) {
                 <div className="p-1">
                     <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
                     <div className="flex items-start">
-                        <div className="flex mb-2">*****</div>
+                        <div className="flex mb-2"><Stars reviews={restaurant.reviews}/></div>
                         <p className="ml-2">
                             {restaurant.reviews.length} review
                             {restaurant.reviews.length === 1 ? "" : "s"}
