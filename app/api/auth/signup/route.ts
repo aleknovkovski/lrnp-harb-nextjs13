de21779c2f31cf4351657cf9ee9e0b26656ff909
Alek Novkovski <alek@greatcode2.com>
2023-03-13 11:43:09 +0100

    Validate received inputs, send error if any invalid

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,7 +1,57 @@
 import {NextRequest, NextResponse} from "next/server";
+import validator from "validator";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const { firstName, lastName, email, phone, city, password } = await request.json();
 
-  return NextResponse.json({ name: firstName + " " + lastName })
+   const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First name is invalid",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last name is invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone number is invalid",
+      },
+      {
+        valid: validator.isLength(city, { min: 1 }),
+        errorMessage: "City is invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      const data = { errorMessage: errors[0] }
+      const json = JSON.stringify(data, null, 2)
+      return new NextResponse (json, { status: 400, headers: {
+          'content-type': 'application/json; charset=utf-8',
+        }});
+    }
+
+  return NextResponse.json({ name: firstName + " " + lastName } )
 }
