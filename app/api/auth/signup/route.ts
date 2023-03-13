59ab9713ed747e3f605934e0603df5c32767a096
Alek Novkovski <alek@greatcode2.com>
2023-03-13 11:50:20 +0100

    Prettier reformat

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,57 +1,62 @@
 import {NextRequest, NextResponse} from "next/server";
 import validator from "validator";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
-  const { firstName, lastName, email, phone, city, password } = await request.json();
+    const {firstName, lastName, email, phone, city, password} = await request.json();
 
-   const errors: string[] = [];
+    const errors: string[] = [];
 
     const validationSchema = [
-      {
-        valid: validator.isLength(firstName, {
-          min: 1,
-          max: 20,
-        }),
-        errorMessage: "First name is invalid",
-      },
-      {
-        valid: validator.isLength(lastName, {
-          min: 1,
-          max: 20,
-        }),
-        errorMessage: "Last name is invalid",
-      },
-      {
-        valid: validator.isEmail(email),
-        errorMessage: "Email is invalid",
-      },
-      {
-        valid: validator.isMobilePhone(phone),
-        errorMessage: "Phone number is invalid",
-      },
-      {
-        valid: validator.isLength(city, { min: 1 }),
-        errorMessage: "City is invalid",
-      },
-      {
-        valid: validator.isStrongPassword(password),
-        errorMessage: "Password is not strong enough",
-      },
+        {
+            valid: validator.isLength(firstName, {
+                min: 1,
+                max: 20,
+            }),
+            errorMessage: "First name is invalid",
+        },
+        {
+            valid: validator.isLength(lastName, {
+                min: 1,
+                max: 20,
+            }),
+            errorMessage: "Last name is invalid",
+        },
+        {
+            valid: validator.isEmail(email),
+            errorMessage: "Email is invalid",
+        },
+        {
+            valid: validator.isMobilePhone(phone),
+            errorMessage: "Phone number is invalid",
+        },
+        {
+            valid: validator.isLength(city, {min: 1}),
+            errorMessage: "City is invalid",
+        },
+        {
+            valid: validator.isStrongPassword(password),
+            errorMessage: "Password is not strong enough",
+        },
     ];
 
     validationSchema.forEach((check) => {
-      if (!check.valid) {
-        errors.push(check.errorMessage);
-      }
+        if (!check.valid) {
+            errors.push(check.errorMessage);
+        }
     });
 
     if (errors.length) {
-      const data = { errorMessage: errors[0] }
-      const json = JSON.stringify(data, null, 2)
-      return new NextResponse (json, { status: 400, headers: {
-          'content-type': 'application/json; charset=utf-8',
-        }});
+        const data = {errorMessage: errors[0]}
+        const json = JSON.stringify(data, null, 2)
+        return new NextResponse(json, {
+            status: 400, headers: {
+                'content-type': 'application/json; charset=utf-8',
+            }
+        });
     }
 
-  return NextResponse.json({ name: firstName + " " + lastName } )
+    return NextResponse.json({name: firstName + " " + lastName})
 }
