9f349c588807ce665c346c1905e730fa798e7c77
Alek Novkovski <alek@greatcode2.com>
2023-03-14 11:03:31 +0100

    Move jwt validation to middleware.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,17 +1,18 @@
 import {NextRequest, NextResponse} from "next/server";
 import * as jose from "jose";
 import jwt from "jsonwebtoken";
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
 
     const bearerToken = request.headers.get("Authorization") as string;
     const token = bearerToken.split(" ")[1];
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const payload = jwt.decode(token) as { email: string };
 
-    try {
-        await jose.jwtVerify(token, secret)
-    } catch (err) {
+    if (!payload.email) {
         const data = {errorMessage: "Unauthorized request"}
         const json = JSON.stringify(data, null, 2)
         return new NextResponse(json, {
@@ -21,7 +22,35 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         })
     }
 
-    const payload = jwt.decode(token) as { email: string };
+    const user = await prisma.user.findUnique({
+        where: {
+            email: payload.email,
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            city: true,
+            phone: true,
+        },
+    });
 
-    return NextResponse.json({yourPayload: payload})
+    if (!user) {
+        const data = {errorMessage: "User not found"}
+        const json = JSON.stringify(data, null, 2)
+        return new NextResponse(json, {
+            status: 401, headers: {
+                'content-type': 'application/json; charset=utf-8',
+            }
+        })
+    }
+
+    return NextResponse.json({
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    phone: user.phone,
+    city: user.city,
+  })
 }
