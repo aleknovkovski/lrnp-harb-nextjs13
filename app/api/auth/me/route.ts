4fd185756a72bdb49b5c8421dc206c25b87fc89d
Alek Novkovski <alek@greatcode2.com>
2023-03-14 01:10:24 +0100

    Verify jwt, send error if no verify

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,9 +1,24 @@
 import {NextRequest, NextResponse} from "next/server";
+import * as jose from "jose";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
 
     const bearerToken = request.headers.get("Authorization") as string;
     const token = bearerToken.split(" ")[1];
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    try {
+        await jose.jwtVerify(token, secret)
+    } catch (err) {
+        const data = {errorMessage: "Unauthorized request"}
+        const json = JSON.stringify(data, null, 2)
+        return new NextResponse(json, {
+            status: 401, headers: {
+                'content-type': 'application/json; charset=utf-8',
+            }
+        })
+    }
 
     return NextResponse.json({yourToken: token})
 }
