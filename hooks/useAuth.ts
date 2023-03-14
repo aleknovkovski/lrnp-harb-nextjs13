27be93dccc3c0949c675baf31f3b50fdd264ecd6
Alek Novkovski <alek@greatcode2.com>
2023-03-14 11:37:44 +0100

    Setup basic-testing signin fn under useAuth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,5 +1,19 @@
+import axios from "axios";
+
 const useAuth = () => {
-    async function signin() {
+    async function signin({email, password}: { email: string; password: string; }) {
+        try {
+            const response = await axios.post(
+                "http://localhost:3000/api/auth/signin",
+                {
+                    email,
+                    password,
+                }
+            );
+            console.log(response)
+        } catch (error: any) {
+            console.log(error)
+        }
     };
 
     async function signup() {
