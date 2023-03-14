dd03122a939aa98725939561a28806d995e77430
Alek Novkovski <alek@greatcode2.com>
2023-03-14 11:22:21 +0100

    Setup useAuth hook skeleton

---
+++ hooks/useAuth.ts
@@ -0,0 +1,19 @@
+const useAuth = () => {
+    async function signin() {
+    };
+
+    async function signup() {
+    };
+
+    async function signout() {
+    };
+
+
+    return {
+        signin,
+        signup,
+        signout,
+    };
+};
+
+export default useAuth;
