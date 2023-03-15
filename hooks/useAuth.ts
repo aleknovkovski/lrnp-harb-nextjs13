547f6f7b073777ec6092bbdd3e841488851801c9
Alek Novkovski <alek@greatcode2.com>
2023-03-15 14:52:06 +0100

    Implement Signup in useAuth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -37,7 +37,55 @@ const useAuth = () => {
         }
     };
 
-    async function signup() {
+    async function signup(
+        {
+            email,
+            password,
+            firstName,
+            lastName,
+            city,
+            phone,
+        }: {
+            email: string;
+            password: string;
+            firstName: string;
+            lastName: string;
+            city: string;
+            phone: string;
+        },
+        handleClose: () => void
+    ) {
+        setAuthState({
+            data: null,
+            error: null,
+            loading: true,
+        });
+        try {
+            const response = await axios.post(
+                "http://localhost:3000/api/auth/signup",
+                {
+                    email,
+                    password,
+                    firstName,
+                    lastName,
+                    city,
+                    phone,
+                }
+            );
+            setAuthState({
+                data: response.data,
+                error: null,
+                loading: false,
+            });
+            handleClose();
+        } catch (error: any) {
+            const errorMessage = error.response.data.errorMessage || error.message || error
+            setAuthState({
+                data: null,
+                error: errorMessage,
+                loading: false,
+            });
+        }
     };
 
     async function signout() {
