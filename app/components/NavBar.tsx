650047264404bfc75de70636a8bc1bd890a87e9e
Alek Novkovski <alek@greatcode2.com>
2023-03-28 21:53:32 +0200

    Use signout fn on navbar signout button

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import AuthModal from "@/app/components/AuthModal";
 import {useContext} from "react";
 import {AuthenticationContext} from "@/app/context/AuthContext";
+import useAuth from "@/hooks/useAuth";
 
 export default function NavBar() {
     const {data, loading} = useContext(AuthenticationContext)
+    const {signout} = useAuth()
+
     return (
         <nav className="bg-white p-2 flex justify-between">
             <Link href="" className="font-bold text-gray-700 text-2xl">
@@ -16,7 +19,7 @@ export default function NavBar() {
                 {loading ? null : (
                     <div className="flex">
                         {data ? (
-                            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">Logout</button>
+                            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3" onClick={signout}>Signout</button>
                         ) : (<>
                             <AuthModal isSignin={true}/>
                             <AuthModal isSignin={false}/>
