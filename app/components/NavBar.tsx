ce7c7a17ecbe31cb315637d39af0af16e5b43f2f
Alek Novkovski <alek@greatcode2.com>
2023-03-28 21:17:38 +0200

    Get AuthorizationContext data into NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from "next/link";
 import AuthModal from "@/app/components/AuthModal";
+import {useContext} from "react";
+import {AuthenticationContext} from "@/app/context/AuthContext";
 
 export default function NavBar() {
+    const {data} = useContext(AuthenticationContext)
     return (
         <nav className="bg-white p-2 flex justify-between">
             <Link href="" className="font-bold text-gray-700 text-2xl">
