3dd0874c61813e38dddeedc6fce11d1751854c97
Alek Novkovski <alek@greatcode2.com>
2024-03-02 19:03:51 +0100

    Add dynamic party size options to ReservationCard
    Imported the 'partySize' data and implemented it in the 'ReservationCard' component. It replaces the previous hard-coded options with a dynamic map function, allowing for easier updates and additions in the future.

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,3 +1,5 @@
+import {partySize} from "@/data";
+
 export function ReservationCard() {
     return <div className="fixed w-[15%] bg-white rounded p-3 shadow">
         <div className="text-center border-b pb-2 font-bold">
@@ -6,8 +8,9 @@ export function ReservationCard() {
         <div className="my-3 flex flex-col">
             <label htmlFor="">Party size</label>
             <select name="" className="py-3 border-b font-light" id="">
-                <option value="">1 person</option>
-                <option value="">2 people</option>
+                {partySize.map((size)=> {
+                    return <option key={size.value} value={size.value}>{size.label}</option>
+                })}
             </select>
         </div>
         <div className="flex justify-between">
