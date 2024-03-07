16633bb0dceb3fdd279526d93fed28375c02f913
Alek Novkovski <alek@greatcode2.com>
2024-03-07 21:05:03 +0100

    Fetch and display restaurant and reservation details in reservation page header
    The restaurant data is now fetched from the database based on the URL slug in the reservation page. Details including its image, name, date, and party size are dynamically displayed in the reservation page header.

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,13 +1,35 @@
 import {Header} from "@/app/reserve/[slug]/components/Header";
 import {Form} from "@/app/reserve/[slug]/components/Form";
+import {PrismaClient, Restaurant} from "@prisma/client";
+import {notFound} from "next/navigation";
 
-export default function ReservationPage() {
+const prisma = new PrismaClient()
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {slug},
+    });
+
+    if (!restaurant) {notFound()}
+
+    return restaurant;
+};
+
+export default async function ReservationPage(
+    {params, searchParams}: {params: { slug: string }; searchParams: any }
+) {
+    const restaurant = await fetchRestaurantBySlug(params.slug)
     return (
         <>
             <article className="border-t h-screen">
                 <div className="py-9 w-3/5 m-auto">
                     {/* HEADER */}
-                    <Header/>
+                    <Header
+                        image={restaurant.main_image}
+                        name={restaurant.name}
+                        date={searchParams.date}
+                        partySize={searchParams.partySize}
+                    />
                     {/* HEADER */}
                     {/* FORM */}
                     <Form/>
