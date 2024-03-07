231f8cba573fff4f478cb121e30ce08877fd45db
Alek Novkovski <alek@greatcode2.com>
2024-03-07 01:09:32 +0100

    Extend ReservationCard with functionality
    The ReservationCard component has been extended by adding a 'slug' prop which allows for fetching and displaying restaurant availability details. Now, the card can accept a query to fetch availability details which is then displayed to the user. This feature involves updates in how state is managed within the component, including selection of date, time, and party size. A circular progress component has also been added as a visual indicator of loading status.

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,17 +1,38 @@
 "use client"
 
-import {partySize, times} from "@/data";
+import {partySize as partySizes, times} from "@/data";
 import DatePicker from "react-datepicker"
 import {useState} from "react";
+import useAvailabilities from "@/hooks/useAvailabilities";
+import {CircularProgress} from "@mui/material";
+import Link from "next/link";
 
-export function ReservationCard({openTime, closeTime}:{openTime:string; closeTime:string}) {
+export function ReservationCard({openTime, closeTime, slug}:{openTime:string; closeTime:string, slug: string}) {
+    const {data, loading, error, fetchAvailabilities} = useAvailabilities()
     const [selectedDate, setSelectedDate] = useState<Date | null>(new Date())
+    const [time, setTime] = useState(openTime)
+    const [partySize, setPartySize] = useState("2")
+    const [day, setDay] = useState(new Date().toISOString().split("T")[0])
+
+    console.log(data)
 
     const handleChangeDate = (date: Date | null) => {
         if(date) {
+            setDay(date.toISOString().split("T")[0])
             return setSelectedDate(date)
         } else {
             return setSelectedDate(null)
+        }
+    }
+
+    const handleClick = () => {
+        if (selectedDate) {
+            fetchAvailabilities({
+                slug,
+                day,
+                time,
+                partySize
+            });
         }
     }
 
@@ -43,9 +64,16 @@ export function ReservationCard({openTime, closeTime}:{openTime:string; closeTim
         </div>
         <div className="my-3 flex flex-col">
             <label htmlFor="">Party size</label>
-            <select name="" className="py-3 border-b font-light" id="">
-                {partySize.map((size)=> {
-                    return <option key={size.value} value={size.value}>{size.label}</option>
+            <select name="" className="py-3 border-b font-light" id="" value={partySize} onChange={(e)=>setPartySize(e.target.value)}>
+                {partySizes.map((size)=> {
+                    return (
+                    <option
+                        key={size.value}
+                        value={size.value}
+                    >
+                        {size.label}
+                    </option>
+                    )
                 })}
             </select>
         </div>
@@ -62,9 +90,13 @@ export function ReservationCard({openTime, closeTime}:{openTime:string; closeTim
             </div>
             <div className="flex flex-col w-[48%]">
                 <label htmlFor="">Time</label>
-                <select name="" id="" className="py-3 border-b font-light">
+                <select name="" id="" className="py-3 border-b font-light" value={time} onChange={(e)=> setTime(e.target.value)}>
                     {filterTimeslotsByOpenHours().map((time)=>
-                        <option key={time.time} value={time.time}>{time.displayTime}</option>
+                        <option
+                            key={time.time}
+                            value={time.time}>
+                            {time.displayTime}
+                        </option>
                     )}
                 </select>
             </div>
@@ -72,9 +104,25 @@ export function ReservationCard({openTime, closeTime}:{openTime:string; closeTim
         <div className="mt-5">
             <button
                 className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
+                onClick={handleClick}
+                disabled={loading}
             >
-                Find a Time
+                {loading ? <CircularProgress color="inherit"/> : "Find a Time"}
             </button>
+            {(data && data.length) ? (
+                <div className="mt-4">
+                    <p className="text-reg">Select a Time</p>
+                    <div className="flex flex-wrap mt-2">
+                        {data.map((time)=> {
+                            return time.available ? <Link href={`/reserve/${slug}?date=${day}T${time.time}&partySize=${partySize}`} className="bg-red-600 cursor-pointer p-2 w-24 text-center text-white mb-3 rounded mr-3">
+                                <p className="text-sm font-bold">
+                                    {time.time}
+                                </p>
+                            </Link> : <p className="bg-gray-300 p-2 w-24 mb-3 rounded mr-3"></p>
+                        })}
+                    </div>
+                </div>
+            ) : null}
         </div>
     </div>;
 }
