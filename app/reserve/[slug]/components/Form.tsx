4ad62a9ab11c28912323ec220682093a7d891c4d
Alek Novkovski <alek@greatcode2.com>
2024-03-07 23:11:15 +0100

    Implement form input handling and validation
    Added state to the reservation form that uses the useState hook for various inputs, including booker name, phone, email, and requests. Introduced a validation process where email and certain fields have a specific required length and the submit button only becomes active when all conditions are fulfilled.

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,37 +1,95 @@
+"use client"
+import {useEffect, useState} from "react";
+import * as React from "react";
+
 export function Form() {
+
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const [inputs, setInputs] = useState({
+        bookerFirstName: "",
+        bookerLastName: "",
+        bookerPhone: "",
+        bookerEmail: "",
+        bookerOccasion: "",
+        bookerRequest: ""
+    })
+
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        const isValidEmail = inputs.bookerEmail.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+
+        if (
+            inputs.bookerFirstName.length > 1 &&
+            inputs.bookerLastName.length > 1 &&
+            isValidEmail &&
+            inputs.bookerPhone.length > 5
+        ) {
+            return setDisabled(false);
+        }
+
+        setDisabled(true);
+    }, [inputs]);
+
     return <div className="mt-10 flex flex-wrap justify-between w-[660px]">
+        {inputs.bookerPhone}
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="First name"
+            name="bookerFirstName"
+            value={inputs.bookerFirstName}
+            onChange={handleChangeInput}
         />
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="Last name"
+            name="bookerLastName"
+            value={inputs.bookerLastName}
+            onChange={handleChangeInput}
         />
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="Phone number"
+            name="bookerPhone"
+            value={inputs.bookerPhone}
+            onChange={handleChangeInput}
         />
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="Email"
+            name="bookerEmail"
+            value={inputs.bookerEmail}
+            onChange={handleChangeInput}
         />
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="Occasion (optional)"
+            name="bookerOccasion"
+            value={inputs.bookerOccasion}
+            onChange={handleChangeInput}
         />
         <input
             type="text"
             className="border rounded p-3 w-80 mb-4"
             placeholder="Requests (optional)"
+            name="bookerRequest"
+            value={inputs.bookerRequest}
+            onChange={handleChangeInput}
         />
         <button
             className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
+            disabled={disabled}
         >
             Complete reservation
         </button>
